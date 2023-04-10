87f4af43a3ca2cbd3f35182b3f87dba2d4fe4ce6
Miloš Stojsavljević <skolsic993@gmail.com>
2023-04-10 19:49:54 +0200

    Make deck factory

--- app.js
+++ app.js
@@ -1,42 +1,45 @@
-const myDeck = {
-  deck: [],
-  drawnCards: [],
-  suits: ["hearts", "diamonds", "spades", "clubs"],
-  values: "2,3,4,5,6,7,8,9,10,J,Q,K,A",
-  initializeDeck() {
-    const { suits, values, deck } = this;
+const makeDeck = () => {
+  return {
+    deck: [],
+    drawnCards: [],
+    suits: ["hearts", "diamonds", "spades", "clubs"],
+    values: "2,3,4,5,6,7,8,9,10,J,Q,K,A",
+    initializeDeck() {
+      const { suits, values, deck } = this;
 
-    for (let value of values.split(",")) {
-      for (let suit of suits) {
-        deck.push({ value, suit });
+      for (let value of values.split(",")) {
+        for (let suit of suits) {
+          deck.push({ value, suit });
+        }
       }
-    }
-  },
-  drawCard() {
-    const card = this.deck.pop();
-    this.drawnCards.push(card);
+    },
+    drawCard() {
+      const card = this.deck.pop();
+      this.drawnCards.push(card);
 
-    return card;
-  },
-  drawMultiple(numCards) {
-    const cards = [];
+      return card;
+    },
+    drawMultiple(numCards) {
+      const cards = [];
 
-    for (let i = 0; i < numCards; i++) {
-      cards.push(this.drawCard());
-    }
+      for (let i = 0; i < numCards; i++) {
+        cards.push(this.drawCard());
+      }
 
-    return cards;
-  },
-  shuffle() {
-    const { deck } = this;
+      return cards;
+    },
+    shuffle() {
+      const { deck } = this;
 
-    for (let i = deck.length - 1; i > 0; i--) {
-      let j = Math.floor(Math.random() * (i + 1));
-      [deck[i], deck[j]] = [deck[j], deck[i]];
-    }
-  },
+      for (let i = deck.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [deck[i], deck[j]] = [deck[j], deck[i]];
+      }
+    },
+  };
 };
 
+const myDeck = makeDeck();
 myDeck.initializeDeck();
 myDeck.shuffle();
 const h1 = myDeck.drawMultiple(2);
