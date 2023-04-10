e0fcb33f78841351414f8db609fdeca47dd767ac
Miloš Stojsavljević <skolsic993@gmail.com>
2023-04-10 19:44:05 +0200

    Make deck together with shuffle

---
+++ app.js
@@ -0,0 +1,38 @@
+const myDeck = {
+  deck: [],
+  drawnCards: [],
+  suits: ["hearts", "diamonds", "spades", "clubs"],
+  values: "2,3,4,5,6,7,8,9,10,J,Q,K,A",
+  initializeDeck() {
+    const { suits, values, deck } = this;
+
+    for (let value of values.split(",")) {
+      for (let suit of suits) {
+        deck.push({ value, suit });
+      }
+    }
+  },
+  drawCard() {
+    const card = this.deck.pop();
+    this.drawnCards.push(card);
+
+    return card;
+  },
+  drawMultiple(numCards) {
+    const cards = [];
+
+    for (let i = 0; i < numCards; i++) {
+      cards.push(this.drawCard());
+    }
+
+    return cards;
+  },
+  shuffle() {
+    const { deck } = this;
+
+    for (let i = deck.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [deck[i], deck[j]] = [deck[j], deck[i]];
+    }
+  },
+};
